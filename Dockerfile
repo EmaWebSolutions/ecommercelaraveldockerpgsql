99580b56c144e59ef17e4a93a32b1211af768855
Your Name <emawebsolutions.g@gmail.com>
2023-10-11 18:04:38 +0100

    second

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+# Use the official PHP 8.2 image as the base image
+FROM php:8.2-fpm
+
+# Install system dependencies and PHP extensions
+RUN apt-get update && apt-get install -y \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    zip \
+    unzip \
+    git \
+    libonig-dev \
+    libzip-dev \
+    postgresql-client \
+    libpq-dev \  
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install gd pdo pdo_mysql mbstring zip exif pdo_pgsql
+
+# Set the working directory in the container
+WORKDIR /var/www/html
+
+# Copy the Laravel application files to the container
+COPY . .
+
+# Clear Laravel's configuration cache
+RUN php artisan config:clear
+
+RUN php artisan config:cache
+
+# Install Composer globally
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install Laravel dependencies, ignoring platform requirements
+RUN composer install --ignore-platform-reqs
+
+
+# Set permissions for Laravel
+RUN chmod -R 775 /var/www/html/storage
+
+
+# Make the entrypoint.sh script executable within the container
+RUN chmod +x ./entrypoint.sh
+
+# Expose port 9000 for PHP-FPM (you can remove this if not needed)
+EXPOSE 9000
+
+# Use the custom entrypoint script as the container command
+CMD ["./entrypoint.sh"]
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Language;
+use App\Models\Menu;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File; // Import the File class
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+      
+
+            try {
+                $language = Setting::where('slug', 'default_language')->first();
+                if ($language) {
+                    $locale = $language->value;
+
+                    $lang = Language::where('locale', $locale)->first();
+                    session(['APP_LOCALE' => $locale, 'lang_dir' => $lang->direction]);
+
+                }
+            } catch (\Exception $e) {
+                //
+            }
+
+             if (File::exists(base_path('migrations_and_seeders_completed.flag'))) {
+            // Code that relies on migrations and seeders here
+            $all_menus = Menu::where('is_static', INACTIVE)->with('submenus')->latest()->get();
+            $allsettings = allsetting();
+            view()->share(['all_menus' => $all_menus, 'allsettings' => $allsettings]);
+        }
+        
+    }
+}
